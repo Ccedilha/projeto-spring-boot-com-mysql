1488f4b53c9a94be1a08e7519a4f210ad9a476af
Ian <ianguilhermepenha@gmail.com>
2026-08-06 21:38:02 -0300

    feat: adicionado metodos http(POST, GET, PUT E DELETE)

---
+++ src/main/java/com/example/projetospringbootcommysql/entity/AlunoEntity.java
@@ -0,0 +1,88 @@
+package com.example.projetospringbootcommysql.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+// Essa anotação diz pro Spring Boot que essa classe vai representar uma tabela no seu banco de dados.
+@Entity
+public class AlunoEntity {
+    @Id //Chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Cria um valor automaticamente no ID
+    private int id;
+
+
+    private String nome;
+    private int idade;
+    private String registroAluno;
+    private String emailInstitucional;
+    private String turma;
+    private String periodo;
+    private String apresentacao;
+
+
+    public String getEmailInstitucional() {
+        return emailInstitucional;
+    }
+
+    public void setEmailInstitucional(String emailInstitucional) {
+        this.emailInstitucional = emailInstitucional;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRegistroAluno() {
+        return registroAluno;
+    }
+
+    public void setRegistroAluno(String registroAluno) {
+        this.registroAluno = registroAluno;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public String getApresentacao() {
+        return apresentacao;
+    }
+
+    public void setApresentacao(String apresentacao) {
+        this.apresentacao = apresentacao;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
